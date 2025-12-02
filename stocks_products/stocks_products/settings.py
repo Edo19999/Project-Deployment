11f1aaaa7e1860541b12261b1d48fac8d87d49f1
User <user@example.com>
2025-12-02 23:52:24 +0300

    initial setup: add root .gitignore; cleanup venv tracking

---
+++ stocks_products/stocks_products/settings.py
@@ -0,0 +1,8 @@
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],  # Если пусто, Django ищет в app/templates (stocks_products/templates — ок)
+        'APP_DIRS': True,  # Должно быть True
+        # ... остальное
+    },
+]
