a996a12122f2f51a5d325073975158750441b4c8
User <user@example.com>
2025-12-06 23:44:59 +0300

    Fix DB_HOST for CI: use localhost for local PostgreSQL service

--- logistic/views.py
+++ logistic/views.py
@@ -1,17 +1,18 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
-
 from .models import Product, Stock
 from .serializers import ProductSerializer, StockSerializer
+from django.http import JsonResponse
 
+def test_view(request):
+    return JsonResponse({'message': 'test ok'})
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [SearchFilter]
     search_fields = ['title', 'description']
-
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
