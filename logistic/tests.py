b467cd0afa09594fca59e67ea9eca508ed21e7a7
User <user@example.com>
2025-12-06 23:11:31 +0300

    Fix CI pipeline

--- logistic/tests.py
+++ logistic/tests.py
@@ -1,12 +1,15 @@
 from django.test import TestCase
-from rest_framework.test import APIClient  # Если APIClient из DRF; иначе from django.test import Client
+from rest_framework.test import APIClient
+
 
 class MyTest(TestCase):
     def test_ok(self):
         self.assertTrue(True)
 
     def test_sample_view(self):
-        url = "/api/v1/test/"  # Исправил "/apf/" на "/api/" — проверьте правильный URL в вашем проекте
-        client = APIClient()  # Добавил (), чтобы создать экземпляр
+        # Проверьте правильный URL в urls.py
+        url = "/api/v1/test/"
+        client = APIClient()
         response = client.get(url)
         self.assertEqual(response.status_code, 200)
+
