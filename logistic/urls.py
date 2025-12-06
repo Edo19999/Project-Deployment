4217a21e0674b7d6bcb636fd5e466421767ef743
User <user@example.com>
2025-12-06 23:40:48 +0300

    Fix DB_HOST for CI: use localhost for local PostgreSQL service

--- logistic/urls.py
+++ logistic/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from . import views
+from .views import ProductViewSet, StockViewSet  # <-- Добавь эту строку
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('stocks', StockViewSet)
 
 urlpatterns = [path('test/', views.test_view)] + router.urls
+
